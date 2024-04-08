6151d1e5dfd0348dc8719dfcf4dec887a7812aa3
Michał Jasiński <michal.jasinski11121@gmail.com>
2024-04-09 00:55:25 +0200

    Update _app.tsx

--- blog-project/src/pages/_app.tsx
+++ blog-project/src/pages/_app.tsx
@@ -1,18 +1,21 @@
+import Head from "next/head";
+import { Layout } from "@/components/UI/Layout/Layout";
 import { NextPage } from 'next';
-import { ReactElement } from 'react';
-import { Layout } from '@/components/UI/Layout/Layout';
-import Head from 'next/head';
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
-const App: NextPage<AppProps> = ({ Component, pageProps }): ReactElement => (
-  <Layout>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
-    <Component {...pageProps} />
-  </Layout>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const ComponentWithPageProps = Component as NextPage;
+
+  return (
+    <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ComponentWithPageProps {...pageProps} />
+    </Layout>
+  );
+};
 
 export default App;
